420082c3c0aff15486b039e73e3b0432626cf0af
Aasif Alam Ansari <aasifi@Aasifs-MacBook-Air.local>
2026-05-11 09:00:39 +0545

    first commit

---
+++ hangmanGame.py
@@ -0,0 +1,37 @@
+import random
+animals = ["cat","dog","ant","goat","donkey"]
+ranWord = random.choice(animals)
+blankSpace = ["_"] * len(ranWord)
+wrongGuess = 0
+guessLetter = []
+
+while wrongGuess <6 and "_" in blankSpace:
+    print(blankSpace)
+    print(6 - wrongGuess)
+    letter = input("guess the letter: ").lower()
+    if len(letter) != 1 or not letter.isalpha():
+        print ("Enter One letter (a to z): ")
+        continue
+
+    if letter in guessLetter:
+        print ("Already Guessed.")
+        continue
+
+    guessLetter.append(letter)
+    found = False
+    for i in range(len(ranWord)):
+        if ranWord[i] == letter:
+            blankSpace[i] = letter
+            found = True
+    
+    if not found:
+        wrongGuess +=1
+        print("Wrong Guess.")
+    else:
+        print("Correct Geuss.")
+
+if "_" not in blankSpace:
+    print("you won!")
+else:
+    print("you lost")
+    print("word was:",ranWord)
