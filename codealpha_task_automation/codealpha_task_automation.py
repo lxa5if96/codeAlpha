419b00bd0019f92783504c491fc070e844df1ebb
Aasif Alam Ansari <aasifi@Aasifs-MacBook-Air.local>
2026-06-09 22:25:44 +0545

    txt file not found problem solve

--- codealpha_task_automation/codealpha_task_automation.py
+++ codealpha_task_automation/codealpha_task_automation.py
@@ -1,12 +1,19 @@
 import re
 
-with open('file_name.txt', 'r') as file:
-    content = file.read()
+try:
+    with open("file_name.txt", "r") as file:
+        content = file.read()
 
-emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', content)
+    emails = re.findall(
+        r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}',
+        content
+    )
 
-with open('email.txt', 'w') as file:
-    for email in emails:
-        file.write(email + "\n")
+    with open("email.txt", "w") as file:
+        for email in emails:
+            file.write(email + "\n")
 
-print("Email address extracted and saved to emails.txt")
+    print("Email addresses extracted and saved to email.txt")
+
+except FileNotFoundError:
+    print("Input file not found. Please check the file name and location.")
